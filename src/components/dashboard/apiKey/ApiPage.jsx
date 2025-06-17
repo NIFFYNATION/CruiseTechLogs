2f01d71b756d79be30198683a4ffb6e509c7f92a
NIFFYNATION <adewaoluwanifemi@gmail.com>
2025-06-17 16:24:21 +0100

    .

--- src/components/dashboard/apiKey/ApiPage.jsx
+++ src/components/dashboard/apiKey/ApiPage.jsx
@@ -61,7 +61,7 @@ const ApiPage = () => {
           
           <motion.button
           whileTap={{ scale: 0.97 }}
-          className="bg-quaternary text-white font-semibold rounded-full px-6 py-3 shadow hover:bg-quaternary/90 transition flex items-center"
+          className="bg-quaternary w-fit text-white font-semibold rounded-full px-6 py-3 shadow hover:bg-quaternary/90 transition flex items-center"
         >
          <img className='w-5 h-5 mr-2 ' src="/icons/add-bold.svg" alt="" />
          <span className="text-sm">
